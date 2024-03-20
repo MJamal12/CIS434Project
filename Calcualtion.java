d6a33fc99d9e42cba29271da5d4fcfddf19b6a41
mysticmanikk <162731540+mysticmanikk@users.noreply.github.com>
2024-03-20 11:47:38 -0400

    Add files via upload

---
+++ Calcualtion.java
@@ -0,0 +1,10 @@
+public class Calcualtion {
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b) throws ArithmeticException{
+        return a/b;
+    }
+}
