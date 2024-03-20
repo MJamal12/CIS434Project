4a55cfa273078b1305a88017c7fba3eb3a316974
MJamal12 <157062827+MJamal12@users.noreply.github.com>
2024-03-20 11:51:17 -0400

    Update Yarosh.java

--- Yarosh.java
+++ Yarosh.java
@@ -5,12 +5,15 @@ int multiplication(int a, int b)
   }
 return a*a;
 }
+
+
 int division(int a, int b){
   if (a>b){
     return a/b;
   }
   return "result is less than 1";
 
+  
   int addition(int a, int b){
     return a+b;
   }
